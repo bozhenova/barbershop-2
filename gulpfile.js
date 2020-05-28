568197b864412e0eb38c54ea3f62afa0fb8b2fb0
Margo <margo.bozhenova@gmail.com>
2020-05-29 00:47:13 +0300

    Fix gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,12 @@ var gulp = require('gulp'),
   postcss = require('gulp-postcss'),
   autoprefixer = require('autoprefixer'),
   minify = require('gulp-csso'),
+  image = require('gulp-image'),
+  uglify = require('gulp-uglify'),
   rename = require('gulp-rename');
 var browserSync = require('browser-sync').create();
 
-gulp.task('scss', function(done) {
+gulp.task('sass', function() {
   return gulp
     .src('source/sass/style.scss')
     .pipe(plumber())
@@ -18,35 +20,47 @@ gulp.task('scss', function(done) {
       })
     )
     .pipe(postcss([autoprefixer()]))
-    .pipe(gulp.dest('source/css'))
+    .pipe(gulp.dest('./dist'))
     .pipe(minify())
     .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('source/css'))
+    .pipe(gulp.dest('./dist'))
     .pipe(browserSync.stream());
-
-  done();
-});
-
-gulp.task('html', function() {
-  return gulp.src('source/*.html').pipe(browserSync.stream());
 });
 
 gulp.task('js', function() {
-  return gulp.src('source/js/*.js').pipe(browserSync.stream());
+  return gulp
+    .src('source/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function(done) {
+gulp.task('html', function() {
+  return gulp
+    .src('source/*.html')
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('image', function() {
+  return gulp
+    .src('source/img/*')
+    .pipe(image())
+    .pipe(gulp.dest('./dist/img'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('serve', ['sass', 'js', 'html', 'image'], function() {
   browserSync.init({
-    server: './source',
+    server: {
+      baseDir: './dist',
+    },
   });
 
-  gulp.watch('source/sass/**/*.scss', gulp.series('scss'));
-  gulp.watch('source/*.html').on('change', () => {
-    browserSync.reload();
-    done();
-  });
-
-  done();
+  gulp.watch('source/sass/*.scss', ['sass']).on('change', browserSync.reload);
+  gulp.watch('source/img/*', ['image']).on('change', browserSync.reload);
+  gulp.watch('source/js/*.js', ['js']).on('change', browserSync.reload);
+  gulp.watch('source/*.html').on('change', browserSync.reload);
 });
 
-gulp.task('default', gulp.series('scss', 'watch'));
+gulp.task('default', ['serve']);
